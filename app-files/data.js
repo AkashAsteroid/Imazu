2bd1dd88bc4aafe6aff70955bc1d8cb94d666db7
Shrestha Akash <akash@ShresthanoMacBook-Pro.local>
2025-05-21 20:57:41 +0900

    initial commit

---
+++ app-files/data.js
@@ -0,0 +1,87 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-r0010046-2",
+      "name": "R0010046 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -2.2960842226355602,
+        "pitch": -0.01930147772098323,
+        "fov": 1.3614897937301778
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6257590823290062,
+          "pitch": 0.27885234074425114,
+          "rotation": 0,
+          "target": "1-r0010047-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-r0010047-2",
+      "name": "R0010047 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -1.0304951698838671,
+        "pitch": -0.03740456772375467,
+        "fov": 1.3614897937301778
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6257465584794062,
+          "pitch": -0.03657110610355474,
+          "rotation": 0,
+          "target": "0-r0010046-2"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
